8871f73148e18e3a06931ea1e4d7ae9e6aa30fac
jollygrace15 <jollygrace.c.4@gmail.com>
2022-02-24 23:04:04 +0000

    added froms/index.js, and hbs file and a route to create a service form.

--- routes/services.js
+++ routes/services.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// import in the Forms
+const { bootstrapField, createProductForm } = require('../forms');
+
 // #1 import in the Product model
 const {Service} = require('../models/index.js')
 
@@ -9,6 +12,13 @@ router.get('/', async (req,res)=>{
     let services = await Service.collection().fetch();
     res.render('services/index', {
         'services': services.toJSON() // #3 - convert collection to JSON
+    })
+})
+
+router.get('/create', async (req, res) => {
+    const serviceForm = createServiceForm();
+    res.render('service/create',{
+        'form': serviceForm.toHTML(bootstrapField)
     })
 })
 
