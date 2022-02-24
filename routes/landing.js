04151487e0fb544a1830a75a70e7421638928399
jollygrace15 <jollygrace.c.4@gmail.com>
2022-02-24 19:16:52 +0000

    added about-us and contact-us hbs

--- routes/landing.js
+++ routes/landing.js
@@ -8,4 +8,15 @@ router.get('/', (req,res)=>{
     res.render('landing/index')
 })
 
+
+router.get('/about-us', (req,res)=>{
+    res.render('landing/about-us')
+})
+
+router.get('/contact-us', (req,res)=>{
+    res.render('landing/contact-us')
+})
+
+
+
 module.exports = router; // #3 export out the router
